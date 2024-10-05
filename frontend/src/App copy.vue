2d88c3861037dec385351c0c7ab28c82ad75efca
Solano Bryan <solanobryan007@gmail.com>
2024-10-04 23:31:03 -0300

    -

--- frontend/src/App copy.vue
+++ frontend/src/App copy.vue
@@ -1,22 +1,23 @@
-<script>
-
-export default{
-  name: 'App',
-  components: {
-
-  }
-}
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  
-    
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div> Hello</div>
-    
- 
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
 
-  
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
 <style scoped>
